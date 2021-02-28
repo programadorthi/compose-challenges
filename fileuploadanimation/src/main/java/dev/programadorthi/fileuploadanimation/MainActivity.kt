bcf6e5ca59a6de59888568f326abf23644898bd7
Thiago Santos <programadorthi@gmail.com>
2021-02-28 15:43:05 -0300

    Finished file upload animation

--- fileuploadanimation/src/main/java/dev/programadorthi/fileuploadanimation/MainActivity.kt
+++ fileuploadanimation/src/main/java/dev/programadorthi/fileuploadanimation/MainActivity.kt
@@ -3,20 +3,10 @@ package dev.programadorthi.fileuploadanimation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationVector1D
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.Easing
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -32,29 +22,29 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.lerp
-import androidx.compose.ui.util.lerp
 import dev.programadorthi.fileuploadanimation.ui.theme.ChallengesTheme
 import kotlin.math.pow
 import kotlinx.coroutines.launch
 
 private const val ANIM_DURATION = 4_000
-private val DecelerateEasing = Easing { fraction -> fraction.pow(2) }
-private val EaseOutCircEasing = CubicBezierEasing(0.075f, 0.82f, 0.165f, 1.0f)
+private val decelerateEasing = Easing { fraction -> fraction.pow(2) }
+private val easeIn = CubicBezierEasing(0.42f, 0f, 1f, 1f)
+private val easeInOutQuintEasing = CubicBezierEasing(0.86f, 0f, 0.07f, 1f)
+private val easeOutCircEasing = CubicBezierEasing(0.075f, 0.82f, 0.165f, 1f)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,23 +64,22 @@ class MainActivity : AppCompatActivity() {
 fun FileUpload() {
     val scope = rememberCoroutineScope()
     val progress = remember { Animatable(initialValue = 0f) }
-    var inProgress by remember { mutableStateOf(false) }
 
     AnimatedContent(
         progress = progress.value,
-        inProgress = inProgress,
+        inProgress = progress.isRunning,
         onClick = {
             scope.launch {
-                progress.snapTo(targetValue = 0f)
-                inProgress = true
-                progress.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(
-                        durationMillis = ANIM_DURATION,
-                        easing = CubicBezierEasing(0.86f, 0f, 0.07f, 1f)
+                progress.apply {
+                    snapTo(targetValue = 0f) // reset before start
+                    animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(
+                            durationMillis = ANIM_DURATION,
+                            easing = easeInOutQuintEasing
+                        )
                     )
-                )
-                inProgress = false
+                }
             }
         }
     )
@@ -126,17 +115,27 @@ fun AnimatedContent(
                 halfCloudSize = halfCloudSize,
                 inProgress = inProgress
             )
-            Cloud(
-                offset = DpOffset(x = -halfCloudSize, y = -boxSize),
+            LeftCloud(
                 cloudSize = cloudSize,
-                contentDescription = "Left cloud"
+                boxSize = boxSize,
+                inProgress = inProgress
             )
-            Cloud(
-                offset = DpOffset(x = boxSize - halfCloudSize, y = boxSize),
+            RightCloud(
                 cloudSize = cloudSize,
-                contentDescription = "Right cloud"
+                boxSize = boxSize,
+                inProgress = inProgress
             )
             Arrow(halfBoxSize = halfBoxSize, inProgress = inProgress)
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawArc(
+                    color = Color.Blue,
+                    startAngle = -90f,
+                    sweepAngle = 360f * progress,
+                    useCenter = false,
+                    alpha = if (progress < 0.98f) 1f else (1f - progress) / (1f - 0.98f),
+                    style = Stroke(width = 8.dp.toPx())
+                )
+            }
         }
         Spacer(modifier = Modifier.height(20.dp))
         Text(
@@ -154,10 +153,93 @@ fun MiddleCloud(
     inProgress: Boolean
 ) {
     val targetValue = halfBoxSize - halfCloudSize
+    val yPosAnim = remember {
+        Animatable(initialValue = targetValue, typeConverter = Dp.VectorConverter)
+    }
+    LaunchedEffect(key1 = inProgress) {
+        if (inProgress) {
+            yPosAnim.animateTo(
+                targetValue = targetValue,
+                animationSpec = keyframes {
+                    durationMillis = ANIM_DURATION
+                    targetValue at 0
+                    targetValue at (ANIM_DURATION * 0.2f).toInt() with FastOutLinearInEasing
+                    targetValue * 5f at (ANIM_DURATION * 0.3f).toInt()
+                    targetValue * -5f at (ANIM_DURATION * 0.3025f).toInt()
+                    targetValue * -5f at (ANIM_DURATION * 0.7f).toInt() with FastOutSlowInEasing
+                }
+            )
+        }
+    }
     Cloud(
-        offset = DpOffset(x = targetValue, y = targetValue + targetValue),
+        offset = DpOffset(x = targetValue, y = yPosAnim.value),
         cloudSize = cloudSize,
         contentDescription = "Middle cloud"
+    )
+}
+
+@Composable
+fun LeftCloud(
+    cloudSize: Dp,
+    boxSize: Dp,
+    inProgress: Boolean
+) {
+    val yPosAnim = remember {
+        Animatable(initialValue = boxSize, typeConverter = Dp.VectorConverter)
+    }
+    LaunchedEffect(key1 = inProgress) {
+        if (inProgress) {
+            yPosAnim.animateTo(
+                targetValue = boxSize,
+                animationSpec = keyframes {
+                    delayMillis = 300
+                    durationMillis = ANIM_DURATION
+                    -boxSize at 0
+                    -boxSize at (ANIM_DURATION * 0.2f).toInt() with easeIn
+                    boxSize at (ANIM_DURATION * 0.4f).toInt()
+                    -boxSize at (ANIM_DURATION * 0.4025f).toInt()
+                    -boxSize at (ANIM_DURATION * 0.5f).toInt() with easeIn
+                    boxSize at (ANIM_DURATION * 0.7f).toInt()
+                }
+            )
+        }
+    }
+    Cloud(
+        offset = DpOffset(x = boxSize * -0.15f, y = yPosAnim.value),
+        cloudSize = cloudSize,
+        contentDescription = "Left cloud"
+    )
+}
+
+@Composable
+fun RightCloud(
+    cloudSize: Dp,
+    boxSize: Dp,
+    inProgress: Boolean
+) {
+    val yPosAnim = remember {
+        Animatable(initialValue = boxSize, typeConverter = Dp.VectorConverter)
+    }
+    LaunchedEffect(key1 = inProgress) {
+        if (inProgress) {
+            yPosAnim.animateTo(
+                targetValue = boxSize,
+                animationSpec = keyframes {
+                    durationMillis = ANIM_DURATION
+                    -boxSize at 0
+                    -boxSize at (ANIM_DURATION * 0.2f).toInt() with easeIn
+                    boxSize at (ANIM_DURATION * 0.4f).toInt()
+                    -boxSize at (ANIM_DURATION * 0.4025f).toInt()
+                    -boxSize at (ANIM_DURATION * 0.5f).toInt() with easeIn
+                    boxSize at (ANIM_DURATION * 0.7f).toInt()
+                }
+            )
+        }
+    }
+    Cloud(
+        offset = DpOffset(x = boxSize * 0.55f, y = yPosAnim.value),
+        cloudSize = cloudSize,
+        contentDescription = "Right cloud"
     )
 }
 
@@ -168,18 +250,16 @@ fun Arrow(halfBoxSize: Dp, inProgress: Boolean) {
         Animatable(initialValue = targetValue, typeConverter = Dp.VectorConverter)
     }
     LaunchedEffect(key1 = inProgress) {
-        if (inProgress.not()) {
-            yPosAnim.snapTo(targetValue)
-        } else {
+        if (inProgress) {
             yPosAnim.animateTo(
                 targetValue = targetValue,
                 animationSpec = keyframes {
                     durationMillis = ANIM_DURATION
-                    targetValue at 0 with DecelerateEasing
-                    targetValue * 1.1f at (ANIM_DURATION * 0.075f).toInt()
-                    targetValue * 1.1f at (ANIM_DURATION * 0.2f).toInt() with EaseOutCircEasing
-                    targetValue * 0.6f at (ANIM_DURATION * 0.7f).toInt()
-                    targetValue at ANIM_DURATION with DecelerateEasing
+                    targetValue at 0 with decelerateEasing
+                    targetValue * 1.15f at (ANIM_DURATION * 0.075f).toInt()
+                    targetValue * 1.15f at (ANIM_DURATION * 0.2f).toInt() with easeOutCircEasing
+                    targetValue * 0.6f at (ANIM_DURATION * 0.8f).toInt() with FastOutSlowInEasing
+                    targetValue at ANIM_DURATION
                 }
             )
         }
